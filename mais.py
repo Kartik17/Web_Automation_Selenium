08dff57069405ad068be89fa61fff703f4dc3c52
Kar_tik_17 <32801164+Kartik17@users.noreply.github.com>
2018-04-25 21:03:04 +0530

    Update mais.py

--- mais.py
+++ mais.py
@@ -10,20 +10,17 @@ import matplotlib.pyplot as plt
 from pandas import ExcelWriter
 
 driver = webdriver.Firefox()
-driver.get('http://10.21.6.100:9090/mais/index.action') #Open mais page
+driver.get('url_name.com') #Open mais page
 driver.implicitly_wait(30)
 
 user_name = driver.find_element_by_name('username') #Find the user name tag element
 user_name.clear() #Clear the initial values
-user_name.send_keys('IS05353')#Change USER ID
-#user_name.send_keys('05')
-#user_name.send_keys('3')
-#user_name.send_keys('53')
+user_name.send_keys('UserID')#Change USER ID
 driver.implicitly_wait(30)
 
 password= driver.find_element_by_name('password')#Find the password tag element
 password.clear()
-password.send_keys('9452737104Ad')#Change USER PASSWORD
+password.send_keys('Password')#Change USER PASSWORD
 driver.implicitly_wait(30)
 
 login = driver.find_element_by_id('login-submit')#Find the login-submit button id and click
@@ -65,11 +62,11 @@ no_of_drwgs.clear()
 no_of_drwgs.send_keys(str(no))
 
 
-driver.find_element_by_xpath("//select[@name='project']/option[text()='CHANDRAYAAN-2']").click()#Change PROJECT
-driver.find_element_by_xpath("//select[@name='model']/option[text()='FLIGHT']").click()#Change MODEL
+driver.find_element_by_xpath("//select[@name='project']/option[text()='Project_Name']").click()#Change PROJECT
+driver.find_element_by_xpath("//select[@name='model']/option[text()='Model No']").click()#Change MODEL
 driver.find_element_by_xpath("//select[@name='inspectionRequired']/option[text()='Yes']").click()#Change INSOECTION REQUIRED/NOT
 driver.find_element_by_name('dateRequiredInput').send_keys('28-02-2018')#Change DATE REQURIED (NOT WORKING)
-driver.find_element_by_xpath("//select[@name='aauthority']/option[text()='SHANKARA A']").click()#Change APPROVING AUTHORITY
+driver.find_element_by_xpath("//select[@name='aauthority']/option[text()='Name of person']").click()#Change APPROVING AUTHORITY
 
 
 #################################################################################################
@@ -115,14 +112,4 @@ for i in range(no):
 	driver.find_element_by_xpath("/html/body/div[5]/div[3]/div/button[1]").click()
 	
 
-#writer = ExcelWriter('mais_new.xlsx')
-#df.to_excel(writer,'Sheet1')
-#writer.save()
-	
-#df.hist()	
-#plt.hist(bins = np.unique(df['Raw Material']))
-#plt.show()
-#size1,2,3
-#id='matquantity'
-#purchaseOrder
-#BUTTON 'ADD','CANCEL'
+
